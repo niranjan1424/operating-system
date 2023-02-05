0e2a0d67269793f634c11196f521c0834a94ab32
niranjan <106364901+niranjan1424@users.noreply.github.com>
2023-02-05 10:42:15 +0530

    Update 17.banker's algorithm.c

--- 17.banker's algorithm.c
+++ 17.banker's algorithm.c
@@ -1,71 +1,140 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
+int max[100][100];
+int alloc[100][100];
+int need[100][100];
+int avail[100];
+int n,r;
+void input();
+void show();
+void cal();
 int main()
 {
-	int n, m, i, j, k;
-	n = 5; 
-	m = 3; 
-	int alloc[5][3] = { { 0, 1, 0 }, 
-						{ 2, 0, 0 }, 
-						{ 3, 0, 2 }, 
-						{ 2, 1, 1 }, 
-						{ 0, 0, 2 } }; 
-
-	int max[5][3] = { { 7, 5, 3 }, 
-					{ 3, 2, 2 },
-					{ 9, 0, 2 },
-					{ 2, 2, 2 },
-					{ 4, 3, 3 } }; 
-
-	int avail[3] = { 3, 3, 2 }; 
-
-	int f[n], ans[n], ind = 0;
-	for (k = 0; k < n; k++) {
-		f[k] = 0;
-	}
-	int need[n][m];
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++)
-			need[i][j] = max[i][j] - alloc[i][j];
-	}
-	int y = 0;
-	for (k = 0; k < 5; k++) {
-		for (i = 0; i < n; i++) {
-			if (f[i] == 0) {
-
-				int flag = 0;
-				for (j = 0; j < m; j++) {
-					if (need[i][j] > avail[j]){
-						flag = 1;
-						break;
-					}
-				}
-
-				if (flag == 0) {
-					ans[ind++] = i;
-					for (y = 0; y < m; y++)
-						avail[y] += alloc[i][y];
-					f[i] = 1;
-				}
-			}
-		}
-	}
-
-	int flag = 1;
-	for(i=0;i<n;i++)
-	{
-	if(f[i]==0)
-	{
-		flag=0;
-		printf("The following system is not safe");
-		break;
-	}
-	}
-	if(flag==1)
-	{
-	printf("Following is the SAFE Sequence\n");
-	for (i = 0; i < n - 1; i++)
-		printf(" P%d ->", ans[i]);
-	printf(" P%d", ans[n - 1]);
-	}
-	return (0);
+int i,j;
+printf("********** Banker's Algo ************\n");
+input();
+show();
+cal();
+getch();
+return 0;
+}
+void input()
+{
+int i,j;
+printf("Enter the no of Processes\t");
+scanf("%d",&n);
+printf("Enter the no of resources instances\t");
+scanf("%d",&r);
+printf("Enter the Max Matrix\n");
+for(i=0;i<n;i++)
+{
+for(j=0;j<r;j++)
+{
+scanf("%d",&max[i][j]);
+}
+}
+printf("Enter the Allocation Matrix\n");
+for(i=0;i<n;i++)
+{
+for(j=0;j<r;j++)
+{
+scanf("%d",&alloc[i][j]);
+}
+}
+printf("Enter the available Resources\n");
+for(j=0;j<r;j++)
+{
+scanf("%d",&avail[j]);
+}
+}
+void show()
+{
+int i,j;
+printf("Process\t Allocation\t Max\t Available\t");
+for(i=0;i<n;i++)
+{
+printf("\nP%d\t ",i+1);
+for(j=0;j<r;j++)
+{
+printf("%d ",alloc[i][j]);
+}
+printf("\t");
+for(j=0;j<r;j++)
+{
+printf("%d ",max[i][j]);
+}
+printf("\t");
+if(i==0)
+{
+for(j=0;j<r;j++)
+printf("%d ",avail[j]);
+}
+}
+}
+void cal()
+{
+int finish[100],temp,need[100][100],flag=1,k,c1=0;
+int safe[100];
+int i,j;
+for(i=0;i<n;i++)
+{
+finish[i]=0;
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<r;j++)
+{
+need[i][j]=max[i][j]-alloc[i][j];
+}
+}
+printf("\n");
+while(flag)
+{
+flag=0;
+for(i=0;i<n;i++)
+{
+int c=0;
+for(j=0;j<r;j++)
+{
+if((finish[i]==0)&&(need[i][j]<=avail[j]))
+{
+c++;
+if(c==r)
+{
+for(k=0;k<r;k++)
+{
+avail[k]+=alloc[i][j];
+finish[i]=1;
+flag=1;
+}
+printf("P%d->",i);
+if(finish[i]==1)
+{
+i=n;
+}
+}
+}
+}
+}
+}
+for(i=0;i<n;i++)
+{
+if(finish[i]==1)
+{
+c1++;
+}
+else
+{
+printf("P%d->",i);
+}
+}
+if(c1==n)
+{
+printf("\n The system is in safe state");
+}
+else
+{
+printf("\n Process are in dead lock");
+printf("\n System is in unsafe state");
+}
 }
