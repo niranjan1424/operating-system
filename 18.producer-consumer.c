8af0f37f1fd4dbcf996725bd6f1c7628d5ac34b3
niranjan <106364901+niranjan1424@users.noreply.github.com>
2023-02-05 10:56:12 +0530

    Update 18.producer-consumer.c

--- 18.producer-consumer.c
+++ 18.producer-consumer.c
@@ -1,82 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-int mutex = 1;
-int full = 0;
-int empty = 10, x = 0;
-
-void producer()
-{
-
-	--mutex;
-
-	++full;
-
-	--empty;
-
-	x++;
-	printf("\nProducer produces"
-		"item %d",
-		x);
-	++mutex;
-}
-
-void consumer()
-{
-	
-	--mutex;
-	--full;
-	++empty;
-	printf("\nConsumer consumes "
-		"item %d",
-		x);
-	x--;
-	++mutex;
-}
-
-
+#include<stdio.h>
+#include<stdlib.h>
+int mutex=1,full=0,empty=3,x=0;
 int main()
 {
-	int n, i;
-	printf("\n1. Press 1 for Producer"
-		"\n2. Press 2 for Consumer"
-		"\n3. Press 3 for Exit");
-
-
-	for (i = 1; i > 0; i++) {
-
-		printf("\nEnter your choice:");
-		scanf("%d", &n);
-
-		
-		switch (n) {
-		case 1:
-		if ((mutex == 1)
-				&& (empty != 0)) {
-				producer();
-			}
-			else {
-				printf("Buffer is full!");
-			}
-			break;
-
-		case 2:
-
-			
-			if ((mutex == 1)
-				&& (full != 0)) {
-				consumer();
-			}
-
-			
-			else {
-				printf("Buffer is empty!");
-			}
-			break;
-
-		
-		case 3:
-			exit(0);
-			break;
-		}
-	}
+ int n;
+ void producer();
+ void consumer();
+ int wait(int);
+ int signal(int);
+ printf("\n1.Producer\n2.Consumer\n3.Exit");
+ while(1)
+ {
+ printf("\nEnter your choice:");
+ scanf("%d",&n);
+ switch(n)
+ {
+ case 1: if((mutex==1)&&(empty!=0))
+ producer();
+ else
+ printf("Buffer is full!!");
+ break;
+ case 2: if((mutex==1)&&(full!=0))
+ consumer();
+ else
+ printf("Buffer is empty!!");
+ break;
+ case 3:
+ exit(0);
+ break;
+ }
+ }
+ return 0;
+}
+int wait(int s)
+{
+ return (--s);
+}
+int signal(int s)
+{
+ return(++s);
+}
+void producer()
+{
+ mutex=wait(mutex);
+ full=signal(full);
+ empty=wait(empty);
+ x++;
+ printf("\nProducer produces the item %d",x);
+ mutex=signal(mutex);
+}
+void consumer()
+{
+ mutex=wait(mutex);
+ full=wait(full);
+ empty=signal(empty);
+ printf("\nConsumer consumes item %d",x);
+ x--;
+ mutex=signal(mutex);
 }
